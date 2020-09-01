de2f2fb099fe8fc10e1c520fa7174b1e573aa384
jasper zoffmann nilsen <www.jasper@live.dk>
2020-09-01 11:15:05 +0200

    Add project files.

---
+++ Opgave 5 Øvelse 1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_5_Øvelse_1
+{
+    public class Assignment1
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get => myVar; 
+            set => myVar = value; 
+        }
+
+        public Assignment1(int i)
+        {
+            MyProperty = i;
+        }
+
+        public int IntFunction(dynamic s)
+        {
+            if (s.GetType() == typeof(int))
+            {
+                return myVar = s;
+            }
+            return 0;
+        }
+    }
+}
